584807599104762a5f4d09742cf66ff0a5e7f816
usernameffas <151457809+usernameffas@users.noreply.github.com>
2026-02-21 17:20:16 +0900

    e

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,13 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // 프론트에서 /api로 보내는 요청을 8787 포트(백엔드)로 배달해줍니다.
+      '/api': {
+        target: 'http://localhost:8787',
+        changeOrigin: true,
+      }
+    }
+  }
 });
